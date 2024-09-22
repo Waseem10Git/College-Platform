364d89ec87a6ce3355ccf1cea22ff15088f3937e
Waseem.Gh <aaa175903@gmail.com>
2024-09-22 14:55:06 +0300

    update server createConnection

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,7 +3,7 @@ const env = require('dotenv');
 
 env.config();
 
-const conn = mysql.createConnection(process.env.MYSQL_URL);
+const conn = mysql.createConnection('mysql://root:HxsnpogUcXIOlgTVXQgRIiUXlLCLLYxU@mysql-ow2_.railway.internal:3306/railway');
 conn.connect(function (err){
     if(err) throw err;
     console.log("Connected To Database :)");
