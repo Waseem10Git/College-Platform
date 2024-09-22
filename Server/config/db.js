d16085c88449662202ddac954984fcf400afac2f
Waseem.Gh <aaa175903@gmail.com>
2024-09-22 14:53:15 +0300

    update server createConnection

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,13 +3,7 @@ const env = require('dotenv');
 
 env.config();
 
-const connectionOptions = mysql.createConnection(process.env.MYSQL_URL);
-
-const conn = mysql.createConnection({
-    ...connectionOptions.config,
-    timezone: 'UTC',
-    flags: ['--max_allowed_packet=64M']
-});
+const conn = mysql.createConnection(process.env.MYSQL_URL);
 conn.connect(function (err){
     if(err) throw err;
     console.log("Connected To Database :)");
