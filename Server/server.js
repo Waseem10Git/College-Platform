e031ef2287d51b85183fe74c44f98135de6b5d19
Waseem.Gh <aaa175903@gmail.com>
2024-09-22 18:52:36 +0300

    Edit server from http to https

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('http');
+const https = require('https');
 const { Server } = require('socket.io');
 const conn = require('./config/db');
 // const createTables = require('./config/createTables');
@@ -31,7 +31,7 @@ const studentMeetingRoutes = require('./routes/studentMeetingRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: '*',
@@ -76,9 +76,9 @@ app.use('/api', studentExamStatusRoutes);
 app.use('/api', studentMeetingRoutes);
 app.use('/api', userRoutes);
 
-
-server.listen(process.env.MYSQLPORT || 4001, () => {
-    console.log(`Server is running on port ${process.env.MYSQLPORT || 4001}`);
+const port = process.env.PORT || 4001;
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 io.on('connection', (socket) => {
